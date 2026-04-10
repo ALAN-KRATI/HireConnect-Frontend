08b71b09a15e78b73878a1d7bf02fdce69302ff2
ALAN-KRATI <alankratisaxena@gmail.com>
2026-04-10 20:34:32 +0530

    set up made

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-slate-800">
+        HireConnect Frontend Started 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
